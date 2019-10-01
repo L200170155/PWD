e1eb0d2e5d82f9fd607a39926e033e84f290509e
L200170155 <47978656+L200170155@users.noreply.github.com>
2019-10-01 21:24:35 +0700

    Add files via upload

---
+++ Modul 3/tugasss.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Program Penjumlahan</title>
+    </head>
+    <body>
+        <form method="POST" action="tugas.php">
+            <p>Nilai A adalah : <input type="text" name="nilaiA" size="3"></p>
+            <p>Nilai B adalah : <input type="text" name="nilaiB" size="3"></p>
+            <p><input type="submit" name="jumlahkan" value="jumlah"></p>
+        </form>
+    </body>
+</html>
